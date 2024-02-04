a344183cbea788159683b7c488dd8da8c612816e
SimonLem <sim.lemaitre@gmail.com>
2024-02-04 07:47:04 +0100

    add links

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,23 @@ function App() {
       <div className="AppContent">
         <h1 className="AppTitle">Build your perfect dev</h1>
         <CharacterEditor />
+        <div className="LinksRow">
+          <p>
+            Project link :{" "}
+            <a
+              className="Links"
+              href="https://github.com/SimonLem/CreateYourDev"
+            >
+              github.com/SimonLem/CreateYourDev
+            </a>
+          </p>
+          <p>
+            Merci CMJ Blue :{" "}
+            <a className="Links" href="https://www.instagram.com/cmj.blue">
+              instagram.com/cmj.blue
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
